a27e1953b10aaf837df469ee00d7d4abfaf026e9
Thais95 <thaiscdaraujo95@gmail.com>
2023-02-09 14:54:32 -0300

    add: variável de ambiente

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -5,9 +5,9 @@ import styles from './CardList.module.css'
 export const CardList = () => {
   return (
     <div className={styles.container}>
-        <Card title={'Avaliação'} url={'http://vemser-dbc.dbccompany.com.br:39000/vemser/avaliacao-front/alunos'}/> 
-        <Card title={'Captação'} url={'http://vemser-dbc.dbccompany.com.br:39000/vemser/captacao-front/candidatos'}/> 
-        <Card title={'Gerencial'} url={'http://vemser-dbc.dbccompany.com.br:39000/vemser/chronos-front/'}/> 
+      <Card title={'Avaliação'} url={`${process.env.REACT_APP_BACKEND_URL}/vemser/avaliacao-front/alunos`} />
+      <Card title={'Captação'} url={`${process.env.REACT_APP_BACKEND_URL}/vemser/captacao-front/candidatos`} />
+      <Card title={'Gerencial'} url={`${process.env.REACT_APP_BACKEND_URL}/vemser/chronos-front/`} />
     </div>
   )
 }
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://vemser-dbc.dbccompany.com.br:39000/vemser/chronos-back/'
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}/vemser/chronos-back/`
 })
 
 export const authApi = axios.create({
-  baseURL: 'http://vemser-dbc.dbccompany.com.br:39000/vemser/usuario-back/'
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}/vemser/usuario-back/`
 })
